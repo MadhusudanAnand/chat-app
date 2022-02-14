5ff7b80a85f1f003df3f8514d8f38704af0fd841
purplevarun <purplevarun@gmail.com>
2022-02-14 12:39:14 +0530

    added public folder to server.js

--- server.js
+++ server.js
@@ -6,5 +6,6 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 const PORT = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, "public")));
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
